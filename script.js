316cee5cb1ee87e50d0a8aec8f17a196bd88968a
User-s <ani.gamcemlidze20@gmail.com>
2023-05-22 18:17:07 +0400

    navigation-bar-hover-update

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const close = document.querySelector(".mobile-close-click");
 
 const arrow = document.querySelector(".arrow-image");
 
-const mainClick = document.querySelector(".main");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const mainClick = document.querySelector(".service-links");
 
 menuBar.addEventListener("click", function () {
   document.querySelector(".mobile-menu").style.display = "block";
@@ -14,7 +16,20 @@ menuBar.addEventListener("click", function () {
 close.addEventListener("click", function () {
   document.querySelector(".mobile-menu").style.display = "none";
 });
-
 arrow.addEventListener("click", function () {
-  document.querySelector("#mobile-list").style.display = "block";
+  const mobileList = document.querySelector("#mobile-list");
+  mobileList.classList.toggle("active");
+  //   document.querySelector(".arrow-image").style.transform = "rotate(0deg)";
+
+  if (mobileList.classList.contains("active")) {
+    document.querySelector(".arrow-image").style.transform = "rotate(0deg)";
+    document.querySelector(".arrow-image").style.bottom = "12%";
+  } else {
+    document.querySelector(".arrow-image").style.bottom = "62%";
+    document.querySelector(".arrow-image").style.transform = "rotate(180deg)";
+  }
+});
+
+mainClick.addEventListener("click", function () {
+  mobileMenu.style.display = "none";
 });
