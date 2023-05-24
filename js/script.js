da3c4dd16385e1021971dfca4f8d9fd5f0a6faf7
User-s <ani.gamcemlidze20@gmail.com>
2023-05-24 23:01:48 +0400

    mobile-menu-changes-update

--- js/script.js
+++ js/script.js
@@ -2,18 +2,27 @@ const menuBar = document.querySelector(".menu-bar");
 
 const close = document.querySelector(".mobile-close-click");
 
-menuBar.addEventListener("click", function () {
+const main = document.getElementsByClassName("main");
 
+menuBar.addEventListener("click", function () {
   document.querySelector(".mobile-menu").classList.add("active");
+  document.querySelector(".mobile-menu").style.display = "block";
 });
+
+for (let i = 0; i < main.length; i++) {
+  main[i].addEventListener("click", function () {
+    document.querySelector(".mobile-menu").style.display = "none";
+  });
+}
 
 close.addEventListener("click", function () {
   document.querySelector(".mobile-menu").classList.remove("active");
 });
 
-document.getElementsByClassName("mob_service_dropdown")[0].addEventListener("click",function(e){
-
-    e.preventDefault()
-   this.classList.toggle("active")
-    document.getElementById("mobile-list").classList.toggle("active")
-})
+document
+  .getElementsByClassName("mob_service_dropdown")[0]
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    this.classList.toggle("active");
+    document.getElementById("mobile-list").classList.toggle("active");
+  });
