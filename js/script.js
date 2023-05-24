e147d79bde666d73773ef6a066947b36b17c5e7d
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2023-05-24 14:09:42 +0400

    update js and css

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const menuBar = document.querySelector(".menu-bar");
+
+const close = document.querySelector(".mobile-close-click");
+
+
+menuBar.addEventListener("click", function () {
+  document.querySelector(".mobile-menu").classList.add("active");
+});
+
+close.addEventListener("click", function () {
+  document.querySelector(".mobile-menu").classList.remove("active");
+});
+
+
+
+
+document.getElementsByClassName("mob_service_dropdown")[0].addEventListener("click",function(e){
+
+    e.preventDefault()
+   this.classList.toggle("active")
+    document.getElementById("mobile-list").classList.toggle("active")
+})
